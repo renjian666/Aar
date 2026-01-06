79e29bdfcdef66c497e6f07bbd1575e4d4c2bb6e
qiangli <1839833132@qq.com>
2026-01-06 13:50:08 +0800

    Diebranch

---
+++ src/test/java/org/niiuiniu.java
@@ -0,0 +1,16 @@
+package org;
+
+public class niiuiniu {
+    public static void main(String[] args) {
+        int a = sub.in(args);
+        System.out.println(a);
+        System.out.println("Hello World!");
+    }
+
+}
+class sub{
+
+    public static  int in(String[] args) {
+        return 100;
+    }
+}

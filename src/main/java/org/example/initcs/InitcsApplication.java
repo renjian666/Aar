b86daf00a59748be93d29472dd1ec70f34ba0bf6
qiangli <1839833132@qq.com>
2026-01-06 13:27:46 +0800

    init

---
+++ src/main/java/org/example/initcs/InitcsApplication.java
@@ -0,0 +1,13 @@
+package org.example.initcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/initcs/InitcsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.initcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
